121cf9f1f7cad85ed2219aae37edad89796ace1a
Tim Franzke <timfranzke2002@gmail.com>
2025-11-02 09:21:42 +0100

    Added Simple Sclae without Text

--- classesDH.py
+++ classesDH.py
@@ -1,4 +1,5 @@
 import numpy as np
+import open3d as o3d
 class Cdh_rot:
     phi = 0
     alpha = 0
@@ -17,11 +18,11 @@ class Cdh_rot:
         self.stepSize = _stepSize
         self.max = _max
         self.min = _min
-        self.calcTrans()
+        self.__calcTrans()
         print("Trans:")
         print(self.tran)
 
-    def calcTrans(self):
+    def __calcTrans(self):
         phi = np.radians(self.phi)
         alpha = np.radians(self.alpha)
         self.tran = np.array([
@@ -34,15 +35,15 @@ class Cdh_rot:
     def makeStep(self):
         self.phi = self.phi + 1 *self.stepSize
         if self.phi < self.max:
-            self.calcTrans()
+            self.__calcTrans()
     def getTrans(self):
         return self.tran
     def setZero(self):
         self.phi = self.min
-        self.calcTrans()
+        self.__calcTrans()
     def setAngle(self, _angle):
         self.phi = _angle
-        self.calcTrans()
+        self.__calcTrans()
 
 class Cdh_trans:
     phi = 0
@@ -63,11 +64,11 @@ class Cdh_trans:
         self.max = _max
         self.min = _min
 
-        self.calcTrans()
+        self.__calcTrans()
         print("Trans:")
         print(self.tran)
 
-    def calcTrans(self):
+    def __calcTrans(self):
         phi = np.radians(self.phi)
         alpha = np.radians(self.alpha)
         self.tran = np.array([
@@ -80,12 +81,45 @@ class Cdh_trans:
     def makeStep(self):
         self.d = self.d + 1 *self.stepSize
         if self.d < self.max:
-            self.calcTrans()
+            self.__calcTrans()
     def getTrans(self):
         return self.tran
     def setZero(self):
         self.d = self.min
-        self.calcTrans()
+        self.__calcTrans()
     def setDist(self, _dist):
         self.d = _dist
-        self.calcTrans()
+        self.__calcTrans()
+
+def drawScale(_Xmax, _Ymax, _Zmax):
+    step = 0
+    ScalePoints = np.empty(shape=(0,3))
+    ScaleLines = np.empty(shape=(0, 2))
+    while step < _Xmax:
+        step += 50
+        ScalePoints = np.append(ScalePoints,  [[step, 20, 0]], axis=0) #axis = 0 ansosnten wird das array geflatted
+        ScalePoints = np.append(ScalePoints, [[step, -20, 0]], axis=0)
+        ScaleLines = np.append(ScaleLines, [[len(ScalePoints)-2, len(ScalePoints)-1]], axis=0)
+    step = 0
+    while step < _Ymax:
+        step += 50
+        ScalePoints = np.append(ScalePoints,  [[20, step, 0]], axis=0) #axis = 0 ansosnten wird das array geflatted
+        ScalePoints = np.append(ScalePoints, [[-20, step, 0]], axis=0)
+        ScaleLines = np.append(ScaleLines, [[len(ScalePoints)-2, len(ScalePoints)-1]], axis=0)
+    step = 0
+    while step < _Zmax:
+        step += 50
+        ScalePoints = np.append(ScalePoints,  [[0, 20, step]], axis=0) #axis = 0 ansosnten wird das array geflatted
+        ScalePoints = np.append(ScalePoints, [[0, -20, step]], axis=0)
+        ScaleLines = np.append(ScaleLines, [[len(ScalePoints)-2, len(ScalePoints)-1]], axis=0)
+
+    ScalePoints = np.append(ScalePoints, [[0, 0, 0]], axis=0) #Haupt Scala
+    ScalePoints = np.append(ScalePoints, [[_Xmax, 0, 0]], axis=0)
+    ScalePoints = np.append(ScalePoints, [[0, _Ymax, 0]], axis=0)
+    ScalePoints = np.append(ScalePoints, [[0, 0, _Zmax]], axis=0)
+
+    ScaleLines = np.append(ScaleLines, [[len(ScalePoints)-4, len(ScalePoints)-3]], axis=0)
+    ScaleLines = np.append(ScaleLines, [[len(ScalePoints)-4, len(ScalePoints)-2]], axis=0)
+    ScaleLines = np.append(ScaleLines, [[len(ScalePoints)-4, len(ScalePoints)-1]], axis=0)
+
+    return o3d.geometry.LineSet(points=o3d.utility.Vector3dVector(ScalePoints),lines=o3d.utility.Vector2iVector(ScaleLines))
--- laodFile.py
+++ laodFile.py
@@ -1,5 +1,6 @@
 import numpy as np
 import open3d as o3d
+import classesDH
 
 # === 1. CSV einlesen ===
 # Ersetze den Dateinamen, falls nötig
@@ -10,8 +11,10 @@ punkte = np.loadtxt(datei, delimiter=",", skiprows=1)
 
 print(f"✅ {punkte.shape[0]} Punkte geladen aus '{datei}'")
 
+lines = classesDH.drawScale(2000, 200, 2000)
+
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(punkte)
 axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100, origin=[0, 0, 0])
 
-o3d.visualization.draw_geometries([pcd, axes])
+o3d.visualization.draw_geometries([pcd, axes, lines])
